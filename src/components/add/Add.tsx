4d322804dcffd84a86b9566f460a82a5e22ebd26
varostica <valentina.arostica@sansano.usm.cl>
2023-08-25 16:49:43 -0400

    my favicon

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -8,12 +8,20 @@ type Props = {
 }
 
 const Add = (props: Props) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //add new row
+        // axios.post(`/${props.slug}`, {})
+
+        props.setOpen(false);
+    }
+
     return (
         <div className="add">
             <div className="modal">
                 <span className="close" onClick={() => props.setOpen(false)}>X</span>
                 <h1>Agregar {props.slug}</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     {props.columns
                     .filter(item=>item.field !== "id" && item.field !== "avatar")
                     .map((column) =>(
@@ -22,7 +30,7 @@ const Add = (props: Props) => {
                             <input type={column.type} placeholder={column.field}/>
                         </div>
                     ))}
-                    
+                    <button>Enviar</button>
                 </form>
             </div>
         </div>
